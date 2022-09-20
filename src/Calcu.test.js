8b66a2c01c690f9b1874e31e5e7f514564b9cddb
Marcelo Salinas <chelo336@gmail.com>
2022-09-19 23:24:14 -0400

    error de devolver la sumas de una cadena

--- src/Calcu.test.js
+++ src/Calcu.test.js
@@ -4,4 +4,7 @@ describe("CalculadoraCadenas", () => {
   it("deberia devolver un numero", () => {
     expect(calculadoraCadenas("3")).toEqual(3);
   });
+  it("deberia devolver la suma de una cadena de dos numero ", () => {
+    expect(calculadoraCadenas("3,5")).toEqual(8);
+  });
 });
